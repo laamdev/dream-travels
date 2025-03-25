d60372df77474c62ac17805cf8fd6e1049b6a90d
Luis Anaya <brothersailorwebdev@gmail.com>
2025-03-25 14:29:38 +0100

    feature: set up tailwind, shadcnu, fetch to api and create trip card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
   const travelsData = await fetch(process.env.NEXT_PUBLIC_API_URL as string);
   const travels = await travelsData.json();
 
-  console.log(JSON.stringify(travels, null, 2));
   return (
     <div className="flex flex-col gap-4">
       {travels.map((travel: Travel) => (
