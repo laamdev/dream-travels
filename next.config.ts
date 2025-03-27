8d04a046317e241269acc535b3b00772574fe110
Luis Anaya <brothersailorwebdev@gmail.com>
2025-03-27 07:59:07 +0100

    fix: allow images form everywhere

--- next.config.ts
+++ next.config.ts
@@ -5,43 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "a.cdn-hotels.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.aarp.net",
-      },
-      {
-        protocol: "https",
-        hostname: "content.skyscnr.com",
-      },
-      {
-        protocol: "https",
-        hostname: "humanidades.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.britannica.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lp-cms-production.imgix.net",
-      },
-      {
-        protocol: "https",
-        hostname: "lp-cms-production.imgix.net",
-      },
-      {
-        protocol: "https",
-        hostname: "miro.medium.com",
-      },
-      {
-        protocol: "https",
-        hostname: "unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
+        hostname: "**",
       },
     ],
   },
